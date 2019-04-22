584edd50cc9019fa4926287658b53c0eb39c40f0
Barranger Ridler <barranger@redbitdev.com>
2019-04-22 10:02:06 -0400

    First cut at customization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Barrangers Dev Blog`,
+    author: `Barranger Ridler`,
+    description: `Developer Blog.`,
+    siteUrl: `https://barranger.dev/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `barranger`,
     },
   },
   plugins: [
